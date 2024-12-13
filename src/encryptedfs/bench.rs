32a223113a0f88f4d2c60374cf3202ad2f66836f
Cristian <ioan.necula@stud.acs.upb.ro>
2024-12-13 16:44:23 +0200

    Refactored bench.rs to use criterion for benchmarks
    Signed-off-by: Cristian <ioan.necula@stud.acs.upb.ro>

--- src/encryptedfs/bench.rs
+++ src/encryptedfs/bench.rs
@@ -1,7 +1,5 @@
 #[allow(unused_imports)]
 use std::str::FromStr;
-#[allow(unused_imports)]
-use test::{black_box, Bencher};
 
 #[allow(unused_imports)]
 use rand::Rng;
@@ -13,19 +11,23 @@ use crate::encryptedfs::{DirectoryEntry, DirectoryEntryPlus, FileType, ROOT_INOD
 #[allow(unused_imports)]
 use crate::test_common::{create_attr, get_fs};
 #[allow(unused_imports)]
+use crate::test_common::block_on;   
+#[allow(unused_imports)]
 use crate::{async_util, test_common};
 
-#[bench]
-fn bench_create(b: &mut Bencher) {
-    test_common::bench("bench_create", 1, false, async {
-        let fs = get_fs().await;
+#[allow(unused_imports)]
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-        let mut i = 1;
-        let i = &mut i;
-        b.iter(|| {
-            black_box({
-                async_util::call_async(async {
-                    let test_file = SecretString::from_str(&format!("test-file-{i}")).unwrap();
+#[allow(dead_code)]
+fn bench_create(c: &mut Criterion) {
+    c.bench_function("bench_create", |b| {
+        block_on(async {
+            let fs = get_fs().await;
+
+            let mut i = 1;
+            b.iter(|| {
+                block_on(async {
+                    let test_file = SecretString::from_str(&format!("test-file-{}", i)).unwrap();
                     let _ = fs
                         .create(
                             ROOT_INODE,
@@ -36,128 +38,148 @@ fn bench_create(b: &mut Bencher) {
                         )
                         .await
                         .unwrap();
-                });
-                *i += 1;
-                *i
-            })
-        });
-    });
-}
-
-#[bench]
-fn bench_exists_by_name(b: &mut Bencher) {
-    test_common::bench("exists_by_name", 1, false, async {
-        let fs = get_fs().await;
-
-        let mut rnd = rand::thread_rng();
-        b.iter(|| {
-            async_util::call_async(async {
-                let _ = fs
-                    .exists_by_name(
-                        ROOT_INODE,
-                        &SecretString::from_str(&format!("test-file-{}", rnd.gen_range(1..100)))
-                            .unwrap(),
-                    )
-                    .unwrap();
+                    i += 1;
+                }, 1);
+                black_box(());
             });
-            black_box(());
-        });
+        }, 1);
     });
 }
 
-#[bench]
-fn bench_find_by_name(b: &mut Bencher) {
-    test_common::bench("bench_find_by_name", 1, false, async {
-        let fs = get_fs().await;
+#[allow(dead_code)]
+fn bench_exists_by_name(c: &mut Criterion) {
+    c.bench_function("exists_by_name", |b| {
+        block_on(async {
+            let fs = get_fs().await;
+            let mut rnd = rand::thread_rng();
 
-        for i in 0..100 {
-            let test_file = SecretString::from_str(&format!("test-file-{i}")).unwrap();
-            let _ = fs
-                .create(
-                    ROOT_INODE,
-                    &test_file,
-                    create_attr(FileType::RegularFile),
-                    false,
-                    false,
-                )
-                .await
-                .unwrap();
-        }
+            b.iter(|| {
+                block_on(async {
+                    let _ = fs
+                        .exists_by_name(
+                            ROOT_INODE,
+                            &SecretString::from_str(&format!("test-file-{}", rnd.gen_range(1..100)))
+                                .unwrap(),
+                        )
+                        .unwrap();
+                }, 1);
+                black_box(());
+            });
+        },1);
+    });
+}
 
-        let mut rnd = rand::thread_rng();
-        b.iter(|| {
-            async_util::call_async(async {
-                let _ = fs.get_attr(ROOT_INODE).await.unwrap();
+#[allow(dead_code)]
+fn bench_find_by_name(c: &mut Criterion) {
+    c.bench_function("bench_find_by_name", |b| {
+        block_on(async {
+            let fs = get_fs().await;
+
+            // Setup: Pre-create files
+            for i in 0..100 {
+                let test_file = SecretString::from_str(&format!("test-file-{}", i)).unwrap();
                 let _ = fs
-                    .find_by_name(
+                    .create(
                         ROOT_INODE,
-                        &SecretString::from_str(&format!("test-file-{}", rnd.gen_range(1..100)))
-                            .unwrap(),
+                        &test_file,
+                        create_attr(FileType::RegularFile),
+                        false,
+                        false,
                     )
                     .await
                     .unwrap();
+            }
+
+            let mut rnd = rand::thread_rng();
+            b.iter(|| {
+                block_on(async {
+                    let _ = fs
+                        .find_by_name(
+                            ROOT_INODE,
+                            &SecretString::from_str(&format!("test-file-{}", rnd.gen_range(1..100)))
+                                .unwrap(),
+                        )
+                        .await
+                        .unwrap();
+                }, 1);
+                black_box(());
             });
-            black_box(());
-        });
+        }, 1);
     });
 }
 
-#[bench]
-fn bench_read_dir(b: &mut Bencher) {
-    test_common::bench("bench_read_dir", 1, false, async {
-        let fs = get_fs().await;
+#[allow(dead_code)]
+fn bench_read_dir(c: &mut Criterion) {
+    c.bench_function("bench_read_dir", |b| {
+        block_on(async {
+            let fs = get_fs().await;
 
-        for i in 0..100 {
-            let test_file = SecretString::from_str(&format!("test-file-{i}")).unwrap();
-            let _ = fs
-                .create(
-                    ROOT_INODE,
-                    &test_file,
-                    create_attr(FileType::RegularFile),
-                    false,
-                    false,
-                )
-                .await
-                .unwrap();
-        }
+            // Setup: Pre-create files
+            for i in 0..100 {
+                let test_file = SecretString::from_str(&format!("test-file-{}", i)).unwrap();
+                let _ = fs
+                    .create(
+                        ROOT_INODE,
+                        &test_file,
+                        create_attr(FileType::RegularFile),
+                        false,
+                        false,
+                    )
+                    .await
+                    .unwrap();
+            }
 
-        b.iter(|| {
-            async_util::call_async(async {
-                let iter = fs.read_dir(ROOT_INODE).await.unwrap();
-                let vec: Vec<DirectoryEntry> = iter.map(|e| e.unwrap()).collect();
-                black_box(vec);
+            b.iter(|| {
+                block_on(async {
+                    let iter = fs.read_dir(ROOT_INODE).await.unwrap();
+                    let vec: Vec<DirectoryEntry> = iter.map(|e| e.unwrap()).collect();
+                    black_box(vec);
+                }, 1);
+                black_box(());
             });
-            black_box(());
-        });
+        }, 1);
     });
 }
 
-#[bench]
-fn bench_read_dir_plus(b: &mut Bencher) {
-    test_common::bench("bench_read_dir_plus", 1, false, async {
-        let fs = get_fs().await;
+#[allow(dead_code)]
+fn bench_read_dir_plus(c: &mut Criterion) {
+    c.bench_function("bench_read_dir_plus", |b| {
+        block_on(async {
+            let fs = get_fs().await;
 
-        for i in 0..100 {
-            let test_file = SecretString::from_str(&format!("test-file-{i}")).unwrap();
-            let _ = fs
-                .create(
-                    ROOT_INODE,
-                    &test_file,
-                    create_attr(FileType::RegularFile),
-                    false,
-                    false,
-                )
-                .await
-                .unwrap();
-        }
+            // Setup: Pre-create files
+            for i in 0..100 {
+                let test_file = SecretString::from_str(&format!("test-file-{}", i)).unwrap();
+                let _ = fs
+                    .create(
+                        ROOT_INODE,
+                        &test_file,
+                        create_attr(FileType::RegularFile),
+                        false,
+                        false,
+                    )
+                    .await
+                    .unwrap();
+            }
 
-        b.iter(|| {
-            async_util::call_async(async {
-                let iter = fs.read_dir_plus(ROOT_INODE).await.unwrap();
-                let vec: Vec<DirectoryEntryPlus> = iter.map(|e| e.unwrap()).collect();
-                black_box(vec);
+            b.iter(|| {
+                block_on(async {
+                    let iter = fs.read_dir_plus(ROOT_INODE).await.unwrap();
+                    let vec: Vec<DirectoryEntryPlus> = iter.map(|e| e.unwrap()).collect();
+                    black_box(vec);
+                }, 1);
+                black_box(());
             });
-            black_box(());
-        });
+        }, 1);
     });
 }
+
+criterion_group!(
+    benches,
+    bench_create,
+    bench_exists_by_name,
+    bench_find_by_name,
+    bench_read_dir,
+    bench_read_dir_plus
+);
+criterion_main!(benches);
