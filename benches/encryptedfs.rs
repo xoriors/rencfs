b6381f49894802a66ad9c861a315c5ad129f367d
Radu Marias <radumarias@gmail.com>
2024-12-16 05:55:50 +0200

    [#122] Create utils for running bench with criterion that uses fs and fixed the ones migrated to criterion

--- benches/encryptedfs.rs
+++ benches/encryptedfs.rs
@@ -1,10 +1,11 @@
 #[allow(unused_imports)]
-use std::str::FromStr;
-
-#[allow(unused_imports)]
 use rand::Rng;
 #[allow(unused_imports)]
 use shush_rs::SecretString;
+#[allow(unused_imports)]
+use std::str::FromStr;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::Arc;
 
 #[allow(unused_imports)]
 use rencfs::encryptedfs::{DirectoryEntry, DirectoryEntryPlus, FileType, ROOT_INODE};
@@ -16,207 +17,183 @@ use rencfs::test_common::{create_attr, get_fs};
 use rencfs::{async_util, test_common};
 
 #[allow(unused_imports)]
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+use rand::thread_rng;
+use rencfs::encryptedfs::EncryptedFs;
+use rencfs::test_common::{run_bench, TestSetup};
 
 #[allow(dead_code)]
-fn create(c: &mut Criterion) {
-    c.bench_function("create", |b| {
-        block_on(
-            async {
-                let fs = get_fs().await;
+fn bench_create(c: &mut Criterion) {
+    run_bench(
+        TestSetup {
+            id: "bench_create",
+            read_only_fs: false,
+        },
+        c,
+        setup_fn,
+        run_fn,
+    );
 
-                let mut i = 1;
-                b.iter(|| {
-                    block_on(
-                        async {
-                            let test_file =
-                                SecretString::from_str(&format!("test-file-{}", i)).unwrap();
-                            let _ = fs
-                                .create(
-                                    ROOT_INODE,
-                                    &test_file,
-                                    create_attr(FileType::RegularFile),
-                                    false,
-                                    false,
-                                )
-                                .await
-                                .unwrap();
-                            i += 1;
-                        },
-                        1,
-                    );
-                    black_box(());
-                });
-            },
-            1,
-        );
-    });
+    async fn setup_fn(_fs: Arc<EncryptedFs>, _atomic: &AtomicU64) {}
+
+    async fn run_fn(fs: Arc<EncryptedFs>, atomic: &AtomicU64) {
+        let i = atomic.fetch_add(1, Ordering::SeqCst);
+        let test_file = SecretString::from_str(&format!("test-file-{}", i)).unwrap();
+        let _ = fs
+            .create(
+                ROOT_INODE,
+                &test_file,
+                create_attr(FileType::RegularFile),
+                false,
+                false,
+            )
+            .await
+            .unwrap();
+    }
 }
 
 #[allow(dead_code)]
-fn exists_by_name(c: &mut Criterion) {
-    c.bench_function("exists_by_name", |b| {
-        block_on(
-            async {
-                let fs = get_fs().await;
-                let mut rnd = rand::thread_rng();
+fn bench_exists_by_name(c: &mut Criterion) {
+    run_bench(
+        TestSetup {
+            id: "bench_exists_by_name",
+            read_only_fs: false,
+        },
+        c,
+        setup_fn,
+        run_fn,
+    );
 
-                b.iter(|| {
-                    block_on(
-                        async {
-                            let _ = fs
-                                .exists_by_name(
-                                    ROOT_INODE,
-                                    &SecretString::from_str(&format!(
-                                        "test-file-{}",
-                                        rnd.gen_range(1..100)
-                                    ))
-                                    .unwrap(),
-                                )
-                                .unwrap();
-                        },
-                        1,
-                    );
-                    black_box(());
-                });
-            },
-            1,
-        );
-    });
+    async fn setup_fn(_fs: Arc<EncryptedFs>, _atomic: &AtomicU64) {}
+
+    async fn run_fn(fs: Arc<EncryptedFs>, _atomic: &AtomicU64) {
+        let _ = fs
+            .exists_by_name(
+                ROOT_INODE,
+                &SecretString::from_str(&format!("test-file-{}", thread_rng().gen_range(1..100)))
+                    .unwrap(),
+            )
+            .unwrap();
+    }
 }
 
 #[allow(dead_code)]
-fn find_by_name(c: &mut Criterion) {
-    c.bench_function("find_by_name", |b| {
-        block_on(
-            async {
-                let fs = get_fs().await;
+fn bench_find_by_name(c: &mut Criterion) {
+    run_bench(
+        TestSetup {
+            id: "bench_find_by_name",
+            read_only_fs: false,
+        },
+        c,
+        setup_fn,
+        run_fn,
+    );
 
-                // Setup: Pre-create files
-                for i in 0..100 {
-                    let test_file = SecretString::from_str(&format!("test-file-{}", i)).unwrap();
-                    let _ = fs
-                        .create(
-                            ROOT_INODE,
-                            &test_file,
-                            create_attr(FileType::RegularFile),
-                            false,
-                            false,
-                        )
-                        .await
-                        .unwrap();
-                }
+    // Setup: Pre-create files
+    async fn setup_fn(fs: Arc<EncryptedFs>, _atomic: &AtomicU64) {
+        for i in 0..100 {
+            let test_file = SecretString::from_str(&format!("test-file-{}", i)).unwrap();
+            let _ = fs
+                .create(
+                    ROOT_INODE,
+                    &test_file,
+                    create_attr(FileType::RegularFile),
+                    false,
+                    false,
+                )
+                .await
+                .unwrap();
+        }
+    }
 
-                let mut rnd = rand::thread_rng();
-                b.iter(|| {
-                    block_on(
-                        async {
-                            let _ = fs
-                                .find_by_name(
-                                    ROOT_INODE,
-                                    &SecretString::from_str(&format!(
-                                        "test-file-{}",
-                                        rnd.gen_range(1..100)
-                                    ))
-                                    .unwrap(),
-                                )
-                                .await
-                                .unwrap();
-                        },
-                        1,
-                    );
-                    black_box(());
-                });
-            },
-            1,
-        );
-    });
+    async fn run_fn(fs: Arc<EncryptedFs>, _atomic: &AtomicU64) {
+        let _ = fs
+            .find_by_name(
+                ROOT_INODE,
+                &SecretString::from_str(&format!("test-file-{}", thread_rng().gen_range(1..100)))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+    }
 }
 
 #[allow(dead_code)]
-fn read_dir(c: &mut Criterion) {
-    c.bench_function("read_dir", |b| {
-        block_on(
-            async {
-                let fs = get_fs().await;
+fn bench_read_dir(c: &mut Criterion) {
+    run_bench(
+        TestSetup {
+            id: "bench_read_dir",
+            read_only_fs: false,
+        },
+        c,
+        setup_fn,
+        run_fn,
+    );
 
-                // Setup: Pre-create files
-                for i in 0..100 {
-                    let test_file = SecretString::from_str(&format!("test-file-{}", i)).unwrap();
-                    let _ = fs
-                        .create(
-                            ROOT_INODE,
-                            &test_file,
-                            create_attr(FileType::RegularFile),
-                            false,
-                            false,
-                        )
-                        .await
-                        .unwrap();
-                }
+    // Setup: Pre-create files
+    async fn setup_fn(fs: Arc<EncryptedFs>, _atomic: &AtomicU64) {
+        for i in 0..100 {
+            let test_file = SecretString::from_str(&format!("test-file-{}", i)).unwrap();
+            let _ = fs
+                .create(
+                    ROOT_INODE,
+                    &test_file,
+                    create_attr(FileType::RegularFile),
+                    false,
+                    false,
+                )
+                .await
+                .unwrap();
+        }
+    }
 
-                b.iter(|| {
-                    block_on(
-                        async {
-                            let iter = fs.read_dir(ROOT_INODE).await.unwrap();
-                            let vec: Vec<DirectoryEntry> = iter.map(|e| e.unwrap()).collect();
-                            black_box(vec);
-                        },
-                        1,
-                    );
-                    black_box(());
-                });
-            },
-            1,
-        );
-    });
+    async fn run_fn(fs: Arc<EncryptedFs>, _atomic: &AtomicU64) {
+        let iter = fs.read_dir(ROOT_INODE).await.unwrap();
+        let _: Vec<DirectoryEntry> = iter.map(|e| e.unwrap()).collect();
+    }
 }
 
 #[allow(dead_code)]
-fn read_dir_plus(c: &mut Criterion) {
-    c.bench_function("read_dir_plus", |b| {
-        block_on(
-            async {
-                let fs = get_fs().await;
+fn bench_read_dir_plus(c: &mut Criterion) {
+    run_bench(
+        TestSetup {
+            id: "bench_read_dir_plus",
+            read_only_fs: false,
+        },
+        c,
+        setup_fn,
+        run_fn,
+    );
 
-                // Setup: Pre-create files
-                for i in 0..100 {
-                    let test_file = SecretString::from_str(&format!("test-file-{}", i)).unwrap();
-                    let _ = fs
-                        .create(
-                            ROOT_INODE,
-                            &test_file,
-                            create_attr(FileType::RegularFile),
-                            false,
-                            false,
-                        )
-                        .await
-                        .unwrap();
-                }
+    // Setup: Pre-create files
+    async fn setup_fn(fs: Arc<EncryptedFs>, _atomic: &AtomicU64) {
+        for i in 0..100 {
+            let test_file = SecretString::from_str(&format!("test-file-{}", i)).unwrap();
+            let _ = fs
+                .create(
+                    ROOT_INODE,
+                    &test_file,
+                    create_attr(FileType::RegularFile),
+                    false,
+                    false,
+                )
+                .await
+                .unwrap();
+        }
+    }
 
-                b.iter(|| {
-                    block_on(
-                        async {
-                            let iter = fs.read_dir_plus(ROOT_INODE).await.unwrap();
-                            let vec: Vec<DirectoryEntryPlus> = iter.map(|e| e.unwrap()).collect();
-                            black_box(vec);
-                        },
-                        1,
-                    );
-                    black_box(());
-                });
-            },
-            1,
-        );
-    });
+    async fn run_fn(fs: Arc<EncryptedFs>, _atomic: &AtomicU64) {
+        let iter = fs.read_dir_plus(ROOT_INODE).await.unwrap();
+        let _: Vec<DirectoryEntryPlus> = iter.map(|e| e.unwrap()).collect();
+    }
 }
 
 criterion_group!(
     benches,
-    create,
-    exists_by_name,
-    find_by_name,
-    read_dir,
-    read_dir_plus
+    bench_create,
+    bench_exists_by_name,
+    bench_find_by_name,
+    bench_read_dir,
+    bench_read_dir_plus
 );
 criterion_main!(benches);
